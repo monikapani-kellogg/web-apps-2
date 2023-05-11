e0b28d60ec5b681b19aeb7a47b32ef50a9be5572
Monika Pani <monika.pani@kellogg.northwestern.edu>
2023-05-11 01:22:50 +0000

    basics of a webapp

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,7 +1,8 @@
 class TacosController < ApplicationController
 
   def index
-    # render tacos/index view
+    @fillings = ["Carnitas", "Al Pastor", "Steak", "Fish", "Veggie"]
+    render :template => "tacos/index"
   end
 
 end
